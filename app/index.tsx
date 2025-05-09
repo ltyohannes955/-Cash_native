e6ef68b0e40a14413a2b9fbaa2ec3f3811699de9
ltyohannes955 <ltyohannes95@gmail.com>
2025-05-09 20:12:03 +0300

    change in the main page

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text className="text-2xl text-red-900">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+      <Text className="text-2xl text-red-900">Edit</Text>
     </View>
   );
 }
